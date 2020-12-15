f0fe312006ea38b6efea02417e18b3ffd18f2221
cactusmachete <ahilya16009@iiitd.ac.in>
2020-12-15 17:30:35 +0530

    Added product controllers and productDAO

---
+++ src/main/java/com/nucleus/product/dao/ProductDAO.java
@@ -0,0 +1,39 @@
+package com.nucleus.product.dao;
+
+import com.nucleus.product.model.Product;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductDAO implements ProductDAOInterface {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private Session getSession(){
+        Session session;
+        try {
+            session = sessionFactory.getCurrentSession();
+        } catch (HibernateException E){
+            session = sessionFactory.openSession();
+        }
+        return session;
+    }
+
+    @Override
+    public List<Product> getProductList() {
+        try(Session session = getSession()) {
+            session.beginTransaction();
+            Query<Product> query = session.createQuery("from Product p");
+            List<Product> productList = query.list();
+            session.getTransaction().commit();
+            return productList;
+        }
+    }
+}
---
+++ src/main/java/com/nucleus/product/dao/ProductDAOInterface.java
@@ -0,0 +1,10 @@
+package com.nucleus.product.dao;
+
+import com.nucleus.product.model.Product;
+
+import java.util.List;
+
+public interface ProductDAOInterface {
+
+    List<Product> getProductList();
+}
