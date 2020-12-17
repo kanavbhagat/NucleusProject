97a9692f40be7ccbd8fdfc2629436d7df8ae5717
jiggylepcha <jigme16045@iiitd.ac.in>
2020-12-17 17:21:58 +0530

    code clean up and cred fixes

--- src/main/java/com/nucleus/config/AppConfig.java
+++ src/main/java/com/nucleus/config/AppConfig.java
@@ -67,9 +67,9 @@ public class AppConfig {
 //        ds.setPassword("qwerty");
 
 
-//        ds.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
-//        ds.setUsername("sys as sysdba");
-//        ds.setPassword("pwd");
+        ds.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+        ds.setUsername("sys as sysdba");
+        ds.setPassword("pwd");
 
 //        ds.setUrl("jdbc:oracle:thin:@localhost:1521/xepdb1");
 //
