75a868834a2240e293026af3ac62d17714a272d6
smita <smitagracyminz27@gmail.com>
2020-12-17 11:10:59 +0530

    added spring form taglib tags to receipt jsp files

--- src/main/java/com/nucleus/config/AppConfig.java
+++ src/main/java/com/nucleus/config/AppConfig.java
@@ -69,6 +69,8 @@ public class AppConfig {
 //        ds.setUsername("nsbt");
 //        ds.setPassword("qwerty");
 
+        ds.setUrl("jdbc:oracle:thin:@localhost:1521:ORCLCDB");
+
         ds.setUsername("c##username");
         ds.setPassword("pwd");
 
