76ccbc37f602f0c8fe563b20a84405dfb1b90ca2
Kuldeep Kumar <k2kuldeep@users.noreply.github.com>
2020-12-17 23:07:28 +0530

    LoanDisbursal related view added

--- src/main/java/com/nucleus/customerservice/loandisbursal/controller/LoanDisbursalController.java
+++ src/main/java/com/nucleus/customerservice/loandisbursal/controller/LoanDisbursalController.java
@@ -7,7 +7,9 @@ import com.nucleus.customerservice.loandisbursal.service.LoanDisbursalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Set;
 
@@ -32,6 +34,24 @@ public class LoanDisbursalController {
             throw new CustomerNotFoundException("CustomerCode NOT found: "+ customerId);
         }
         return loanApplications;
+    }
+
+    @GetMapping(path = "/loandisbursalDetails")
+    public ModelAndView getLoanDisbursals(@RequestParam("loanApplicationNumber") int loanApplicationId){
+        LoanApplication loanApplication= loanDisbursalService.getLoanDetails(loanApplicationId);
+        if(loanApplication==null){
+            return new ModelAndView("ErrorPage", "Message", "loanApplication NOT found: "+ loanApplicationId);
+        }
+        return new ModelAndView("LoanDisbursalDetails", "loanApp", loanApplication);
+    }
+
+    @GetMapping(path = "/customerloandisbursal")
+    public ModelAndView getLoanDisbursalsByCustomerId(@RequestParam("customerCode") String customerId){
+        Set<LoanApplication> loanApplications=loanDisbursalService.getCustomerLoanDetails(customerId);
+        if(loanApplications.isEmpty()){
+            return new ModelAndView("ErrorPage", "Message", "CustomerCode NOT found: "+ customerId);
+        }
+        return new ModelAndView("CustomerLoanDisbursal", "allLoanApps", loanApplications);
     }
 
 }
