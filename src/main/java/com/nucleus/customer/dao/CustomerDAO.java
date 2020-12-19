25fa88c4794f756801dba7008e67a5446e1dfd82
hemantsahay98 <41349038+hemantsahay98@users.noreply.github.com>
2020-12-20 00:21:49 +0530

    customer updated

--- src/main/java/com/nucleus/customer/dao/CustomerDAO.java
+++ src/main/java/com/nucleus/customer/dao/CustomerDAO.java
@@ -5,6 +5,7 @@ import com.nucleus.loanapplications.model.LoanApplications;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -58,6 +59,67 @@ public class CustomerDAO implements CustomerDaoInterface{
             }
 
         }
+    }
+    @Override
+    public List<Customer> listCustomer() {
+        try(Session session = getSession()) {
+            session.beginTransaction();
+            Query<Customer> query = session.createQuery("from Customer c");
+            List<Customer> customerList = query.list();
+            session.getTransaction().commit();
+            return customerList;
+        }
+    }
+
+
+
+
+    @Override
+    public boolean removeCustomer(Customer customer) {
+        try(Session session=getSession()){
+            session.beginTransaction();
+            try {
+                session.remove(customer);
+                session.getTransaction().commit();
+                return true;
+            } catch (HibernateException e){
+                e.printStackTrace();
+                session.getTransaction().rollback();
+                return false;
+            }
+
+        }
+
+    }
+
+    @Override
+    public boolean removeCustomer(String id) {
+        try(Session session=getSession()){
+            session.beginTransaction();
+            try {
+                Customer customer = session.get(Customer.class , id);
+                session.remove(customer);
+                session.getTransaction().commit();
+                return true;
+            } catch (HibernateException e){
+                e.printStackTrace();
+                session.getTransaction().rollback();
+                return false;
+            }
+
+        }
+    }
+
+    @Override
+    public Customer getCustomerById(String id) {
+
+        try(Session session = getSession()) {
+            session.beginTransaction();
+            Customer customer = session.get(Customer.class , id);
+            return customer;
+        }
+
+
     }
     public List<LoanApplications> getCustomerLoanDetails(String customerCode){
         Session session = sessionFactory.openSession();
