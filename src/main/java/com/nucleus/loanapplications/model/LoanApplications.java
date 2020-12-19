e5e687019b0656e4c38dca0b6f0a636b6374b51f
Deepak16031 <deepak16031@iiitd.ac.in>
2020-12-19 17:20:40 +0530

    loan Application some  class fields are nullable

--- src/main/java/com/nucleus/loanapplications/model/LoanApplications.java
+++ src/main/java/com/nucleus/loanapplications/model/LoanApplications.java
@@ -45,22 +45,22 @@ public class LoanApplications {
     @Column(name = "installment_due_date",nullable = false)
     private LocalDate installmentDueDate;
 
-    @Column(name = "create_date",nullable = false)
+    @Column(name = "create_date")
     private LocalDate createDate;
 
-    @Column(name = "created_by", nullable = false, length = 30)
+    @Column(name = "created_by", length = 30)
     private String createdBy;
 
-    @Column(name = "modified_date",nullable = false)
+    @Column(name = "modified_date")
     private LocalDate modifiedDate;
 
-    @Column(name = "modified_by", nullable = false, length = 30)
+    @Column(name = "modified_by",  length = 30)
     private String modifiedBy;
 
-    @Column(name = "authorized_date",nullable = false)
+    @Column(name = "authorized_date")
     private LocalDate authorizedDate;
 
-    @Column(name = "authorized_by", nullable = false, length = 30)
+    @Column(name = "authorized_by", length = 30)
     private String authorizedBy;
 
     public Integer getLoanApplicationNumber() {
