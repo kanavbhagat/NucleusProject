b3e4a129e9497c4f4d92f872fa2386ab6c7071bb
Apurv <apurv.karpatne@gmail.com>
2020-12-23 23:33:43 +0530

    Team-2 Testing

--- src/main/java/com/nucleus/config/AppConfig.java
+++ src/main/java/com/nucleus/config/AppConfig.java
@@ -74,15 +74,12 @@ public class AppConfig {
 
        /* ds.setUrl("jdbc:oracle:thin:@localhost:1521/pdborcl");
         ds.setUsername("nsbt");
-        ds.setPassword("qwerty");*/
-
-        ds.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
-        ds.setUsername("apk");
-        ds.setPassword("abcd1234");
-
+        ds.setPassword("qwerty");
+//        ds.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+//        ds.setUsername("c##asaf");
+//        ds.setPassword("asaf");
 //        ds.setUsername("c##username");
 //        ds.setPassword("pwd");
-
          /*ds.setUrl("jdbc:oracle:thin:@localhost:1521:ORCLCDB");
          ds.setUsername("c##username");
          ds.setPassword("pwd");*/
@@ -91,31 +88,37 @@ public class AppConfig {
 //        ds.setUsername("sys as sysdba");
 //       ds.setPassword("gyanesh10");
 
+
+        /* ds.setUrl("jdbc:oracle:thin:@localhost:1521:ORCLCDB");
+         ds.setUsername("c##username");
+         ds.setPassword("pwd");*/
+
 //        ds.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
 //        ds.setUsername("sys as sysdba");
 //        ds.setPassword("pwd");
 
-//        ds.setUrl("jdbc:oracle:thin:@localhost:1521/xepdb1");
-//        ds.setUsername("MYUSERNAME");
-//        ds.setPassword("MYPASSWORD");
+
 //        ds.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
 //       ds.setUsername("megha");
 //       ds.setPassword("megha");
+//        ds.setUrl("jdbc:oracle:thin:@localhost:1521/xepdb1");
+//        ds.setUsername("MYUSERNAME");
+//        ds.setPassword("MYPASSWORD");
+
 
 //         ds.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
 //         ds.setUsername("megha");
 //         ds.setPassword("megha");
 
 
-
-          /*   ds.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+        /*   ds.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
              ds.setUsername("system");
              ds.setPassword("hemant12345");*/
 
        /* ds.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
         ds.setUsername("sys as sysdba");
         ds.setPassword("gkul0289");*/
-            return ds;
+        return ds;
 
     }
 
