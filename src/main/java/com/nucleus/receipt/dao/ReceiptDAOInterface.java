6b58c90e9599843c1e6a54689f818998dc6bb315
cactusmachete <ahilya16009@iiitd.ac.in>
2020-12-23 20:30:19 +0530

    Added documentation and fixed deprecated method use

--- src/main/java/com/nucleus/receipt/dao/ReceiptDAOInterface.java
+++ src/main/java/com/nucleus/receipt/dao/ReceiptDAOInterface.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface ReceiptDAOInterface {
 
     Boolean createNewReceipt(Receipt receipt);
-    List<Object> receiptSearch(String receiptType, String receiptBasis, Integer accountNumber, Integer receiptNo);
+    List<Receipt> receiptSearch(String receiptType, String receiptBasis, Integer accountNumber, Integer receiptNo);
     List<Receipt> getReceiptList();
     public Boolean updateReceipt(Receipt receipt);
     public Receipt getReceipt(Integer receiptId);
