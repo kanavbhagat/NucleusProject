b28498a3ab3267540d1b9849860873e2aa3b2f0a
Kanav bhagat <kanav16046@iiitd.ac.in>
2020-12-23 23:56:56 +0530

    Revert "loan createdBy from system"

--- src/main/java/com/nucleus/login/logindetails/LoginDetailsImpl.java
+++ src/main/java/com/nucleus/login/logindetails/LoginDetailsImpl.java
@@ -2,9 +2,7 @@ package com.nucleus.login.logindetails;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
 
-@Service
 public class LoginDetailsImpl {
 
     public String getUserName()
