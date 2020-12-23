e45c3741dd774451dabd323415c6349d6588fe71
Apurv <apurv.karpatne@gmail.com>
2020-12-24 04:22:54 +0530

    Added Team-2 Test Classes

--- src/main/java/com/nucleus/eligibiltyparameter/database/EligibilityParameterDAOImpl.java
+++ src/main/java/com/nucleus/eligibiltyparameter/database/EligibilityParameterDAOImpl.java
@@ -8,6 +8,7 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NoResultException;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -33,15 +34,14 @@ public class EligibilityParameterDAOImpl implements EligibilityParameterDAO {
     public List<EligibilityParameter> getAll() {
 
         List<EligibilityParameter> eligibilityParameterList;
-        try {
-            Session session = getSession();
+        try (Session session = getSession()){
             session.beginTransaction();
             Query<EligibilityParameter> query = session.createQuery("from EligibilityParameter e", EligibilityParameter.class);
             eligibilityParameterList = query.getResultList();
             session.getTransaction().commit();
-            session.close();
         } catch (Exception exception) {
             eligibilityParameterList = null;
+            System.out.println(exception.getMessage());
         }
         return eligibilityParameterList;
     }
@@ -53,17 +53,14 @@ public class EligibilityParameterDAOImpl implements EligibilityParameterDAO {
      */
     @Override
     public boolean insertParameter(EligibilityParameter eligibilityParameter) {
-
         boolean success;
-        try {
-            Session session = getSession();
+        try (Session session = getSession()) {
             session.beginTransaction();
             session.save(eligibilityParameter);
-            success=true;
             session.getTransaction().commit();
-            session.close();
+            success = true;
         } catch (Exception exception) {
-            success=false;
+            success = false;
             System.out.println(exception.getMessage());
         }
         return success;
@@ -77,14 +74,13 @@ public class EligibilityParameterDAOImpl implements EligibilityParameterDAO {
     @Override
     public EligibilityParameter getOneEligibilityParameter(String parameterCode) {
         EligibilityParameter eligibilityParameter;
-        try {
-            Session session = getSession();
+        try (Session session = getSession()) {
             session.beginTransaction();
-            Query<EligibilityParameter> query = session.createQuery("from EligibilityParameter e where e.parameterCode=?1", EligibilityParameter.class);
+            Query<EligibilityParameter> query;
+            query = session.createQuery("from EligibilityParameter e where e.parameterCode=?1", EligibilityParameter.class);
             query.setParameter(1, parameterCode);
             eligibilityParameter = query.getSingleResult();
             session.getTransaction().commit();
-            session.close();
         } catch (Exception exception) {
             eligibilityParameter = null;
             System.out.println(exception.getMessage());
@@ -101,15 +97,13 @@ public class EligibilityParameterDAOImpl implements EligibilityParameterDAO {
     public boolean deleteEligibilityParameter(String parameterCode) {
         boolean success;
         EligibilityParameter eligibilityParameter = getOneEligibilityParameter(parameterCode);
-        try {
-            Session session = getSession();
+        try (Session session = getSession()) {
             session.beginTransaction();
             session.delete(eligibilityParameter);
-            success=true;
             session.getTransaction().commit();
-            session.close();
+            success = true;
         } catch (Exception exception) {
-            success=false;
+            success = false;
             System.out.println(exception.getMessage());
         }
         return success;
@@ -122,14 +116,10 @@ public class EligibilityParameterDAOImpl implements EligibilityParameterDAO {
      */
     @Override
     public boolean editParameter(EligibilityParameter eligibilityParameter) {
-        if(eligibilityParameter!=null) {
-            try {
-                Session session = getSession();
+        boolean success;
+        if (eligibilityParameter != null && eligibilityParameter.getParameterCode()!=null) {
+            try (Session session = getSession()) {
                 session.beginTransaction();
-
-                Query query1 = session.createQuery("update EligibilityParameter e set e.parameterName = ?1 , " +
-                        "e.parameterDescription = ?2 , e.minValue = ?3 , e.maxValue = ?4 , e.modifiedBy = ?5 ," +
-                        "e.modifiedDate = ?6 , e.status = ?7 where e.parameterCode = ?8");
 
                 String name = eligibilityParameter.getParameterName();
                 String desc = eligibilityParameter.getParameterDescription();
@@ -139,6 +129,9 @@ public class EligibilityParameterDAOImpl implements EligibilityParameterDAO {
                 String status = eligibilityParameter.getStatus();
                 String code = eligibilityParameter.getParameterCode();
 
+                Query query1 = session.createQuery("update EligibilityParameter e set e.parameterName = ?1 , " +
+                        "e.parameterDescription = ?2 , e.minValue = ?3 , e.maxValue = ?4 , e.modifiedBy = ?5 ," +
+                        "e.modifiedDate = ?6 , e.status = ?7 where e.parameterCode = ?8");
                 query1.setParameter(1, name);
                 query1.setParameter(2, desc);
                 query1.setParameter(3, min);
@@ -149,19 +142,16 @@ public class EligibilityParameterDAOImpl implements EligibilityParameterDAO {
                 query1.setParameter(8, code);
 
                 query1.executeUpdate();
-
-
                 session.getTransaction().commit();
-                session.close();
-                return true;
+                success = true;
             } catch (Exception exception) {
+                success = false;
                 System.out.println(exception.getMessage());
-                return false;
             }
+        } else {
+            success = false;
         }
-        else {
-            return false;
-        }
+        return success;
     }
 
     /**
@@ -173,22 +163,24 @@ public class EligibilityParameterDAOImpl implements EligibilityParameterDAO {
      */
     public boolean updateStatus(String parameterCode, String newStatus,String authorizedBy) {
         boolean updateStatus;
-        try {
-            Session session = getSession();
-            session.beginTransaction();
-            EligibilityParameter eligibilityParameter = getOneEligibilityParameter(parameterCode);
-            eligibilityParameter.setAuthorizedBy(authorizedBy);
-            eligibilityParameter.setAuthorizedDate(LocalDate.now());
-            eligibilityParameter.setStatus(newStatus);
-            session.update(eligibilityParameter);
-            session.getTransaction().commit();
-            updateStatus = true;
-            session.close();
-        } catch (Exception exception) {
-            updateStatus = false;
-            System.out.println(exception.getMessage());
+        EligibilityParameter eligibilityParameter = getOneEligibilityParameter(parameterCode);
+        if (eligibilityParameter != null) {
+            try (Session session = getSession()) {
+                session.beginTransaction();
+                eligibilityParameter.setAuthorizedBy(authorizedBy);
+                eligibilityParameter.setAuthorizedDate(LocalDate.now());
+                eligibilityParameter.setStatus(newStatus);
+                session.update(eligibilityParameter);
+                session.getTransaction().commit();
+                updateStatus = true;
+            } catch (Exception exception) {
+                updateStatus = false;
+                System.out.println(exception.getMessage());
+            }
+            return updateStatus;
+        } else {
+            return false;
         }
-        return updateStatus;
     }
 
     /**
@@ -198,14 +190,12 @@ public class EligibilityParameterDAOImpl implements EligibilityParameterDAO {
     @Override
     public List<EligibilityParameter> getApprovedParameters(){
         List<EligibilityParameter> eligibilityParameterList;
-        try {
-            Session session = getSession();
+        try (Session session = getSession()){
             session.beginTransaction();
-            Query query = session.createQuery("from EligibilityParameter e where e.status= ?1");
+            Query<EligibilityParameter> query = session.createQuery("from EligibilityParameter e where e.status= ?1", EligibilityParameter.class);
             query.setParameter(1, "Approved");
             eligibilityParameterList = query.getResultList();
             session.getTransaction().commit();
-            session.close();
         } catch (Exception exception) {
             eligibilityParameterList = null;
             System.out.println(exception.getMessage());
