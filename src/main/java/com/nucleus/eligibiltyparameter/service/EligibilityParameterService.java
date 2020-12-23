64431e10fc1d875200a65f7e002a6bf237e2250e
Apurv <apurv.karpatne@gmail.com>
2020-12-23 10:57:46 +0530

    Eligibility Parameter Service

---
+++ src/main/java/com/nucleus/eligibiltyparameter/service/EligibilityParameterService.java
@@ -0,0 +1,17 @@
+package com.nucleus.eligibiltyparameter.service;
+
+import com.nucleus.eligibiltyparameter.model.EligibilityParameter;
+
+import java.util.List;
+
+public interface EligibilityParameterService {
+    public List<EligibilityParameter> getAll() ;
+    public String insertParameter(EligibilityParameter eligibilityParameter);
+    public EligibilityParameter getOneEligibilityParameter(String parameterCode);
+    public String deleteEligibilityParameter(String parameterCode);
+    public boolean editParameter(EligibilityParameter eligibilityParameter);
+    public boolean updateStatus(String parameterCode,String newStatus,String authorizedBy);
+}
+
+
+
